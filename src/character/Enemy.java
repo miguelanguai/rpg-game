90386f8e6821c7051acc542f557493753fc9aeea
Miguel Angel <miguelangelguaivi@gmail.com>
2024-10-09 19:13:24 +0200

    feat: create method to generate enemy
    this method allows to generate enemies in a random way, depending on the level of the player, and if we want it to be a final boss or a regular enemy

--- src/character/Enemy.java
+++ src/character/Enemy.java
@@ -1,7 +1,10 @@
 package character;
 
+import java.util.Random;
+
 public class Enemy extends Character{
     private Object object;
+
 
     public Enemy(Object object) {
         this.object = object;
@@ -20,5 +23,68 @@ public class Enemy extends Character{
         return "Enemy{" +
                 "object=" + object +
                 '}';
+    }
+
+    /**
+     * genera enemigos. Depende de si es jefe final o no, tiene más o menos dificultad.
+     * @param isBoss depende de si se quiere qeu el enemigo generado sea un jefe final o no
+     * @param object el objeto que entregará al jugador una vez es derrotado
+     * @param playerLevel el nivel del jugador para tomar como referencia
+     * @return
+     */
+    public static Enemy generateEnemy(boolean isBoss, Object object, int playerLevel, String enemyName){
+        Enemy enemy = new Enemy(object);
+        enemy.setName(enemyName);
+
+        if (isBoss){
+            playerLevel+= (int) (playerLevel*0.1);
+            System.out.println(playerLevel);
+            addAtributes(playerLevel, enemy);
+        }else{
+            playerLevel= (int) (playerLevel*0.8);
+            System.out.println(playerLevel);
+            addAtributes(playerLevel, enemy);
+        }
+        return enemy;
+    }
+
+    /**
+     * añade atributos al enemigo de manera randomizada. Al menos tiene un punto de salud.
+     * Cuando acaba el bucle, multiplica por 4 los puntos de vida para que tenga un minimo de vida el enemigo
+     * Setea la salud restante inicial (que está entera) y el estado a normal
+     * @param enemyLevel
+     * @param character
+     * @return
+     */
+    public static Character addAtributes(int enemyLevel, Character character){
+        Random r = new Random();
+        character.setTotalHealth(1);
+        for(int i=0;i<enemyLevel/2;i++){
+            int attributeToAffect=r.nextInt(5);
+
+            switch (attributeToAffect){
+                case 0:
+                    character.setTotalHealth(character.getTotalHealth()+1);
+                    break;
+                case 1:
+                    character.setAttack(character.getAttack()+1);
+                    break;
+                case 2:
+                    character.setMagic(character.getMagic()+1);
+                    break;
+                case 3:
+                    character.setSpeed(character.getSpeed()+1);
+                    break;
+                case 4:
+                    character.setDefense(character.getDefense()+1);
+                    break;
+
+            }
+        }
+        character.setTotalHealth(character.getTotalHealth()*4);
+        character.setLeftHealth(character.getTotalHealth());
+        character.setStatus(Status.NORMAL);
+        System.out.println("Attributes given to the enemy");
+        return character;
     }
 }
